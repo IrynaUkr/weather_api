2e60c85a77433e13ab19c43f825ce3f05abbb4a0
irinapodoprigora <ukrira@gmail.com>
2023-03-25 07:52:54 +0000

    Weather Controller deleted

--- src/main/java/com/techreturners/weatherapi/model/Condition.java
+++ src/main/java/com/techreturners/weatherapi/model/Condition.java
@@ -1,9 +1,13 @@
 package com.techreturners.weatherapi.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Condition {
     String text;
     String icon;
